5c2d9391d425a48efdf8edd52531e50170eaa759
Abukmayen <abukmayen123@gmail.com>
2023-09-18 05:54:53 -0700

    0-statuscode.js

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -9,6 +9,6 @@ const url = process.argv[2];
 // make a GET request
 request
 .get(url)
-.on('response', function (reponse) {
-	console.log('code:', response.statusCode)
+.on('response', function (response) {
+    console.log('code:', response.statusCode)
 })
